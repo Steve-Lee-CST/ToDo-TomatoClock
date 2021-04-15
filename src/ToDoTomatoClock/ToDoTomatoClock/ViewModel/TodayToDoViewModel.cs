f1011c8c36fbd8f4ce672a43335a990a3f55823b
Steve-Lee-CST <steve.lee.cst@gmail.com>
2021-04-15 17:30:29 +0800

    add view-viewmodel

---
+++ src/ToDoTomatoClock/ToDoTomatoClock/ViewModel/TodayToDoViewModel.cs
@@ -0,0 +1,34 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoTomatoClock.ViewModel
+{
+    public class TodayToDoViewModel : ViewModelBase
+    {
+        public TodayToDoViewModel()
+        {
+            InitLocalProperty();
+            InitBindingProperty();
+            InitCommand();
+        }
+
+        private void InitLocalProperty()
+        {
+
+        }
+
+        private void InitBindingProperty()
+        {
+
+        }
+
+        private void InitCommand()
+        {
+
+        }
+
+        
+    }
+}
---
+++ src/ToDoTomatoClock/ToDoTomatoClock/ViewModel/TomatoClockViewModel.cs
@@ -0,0 +1,32 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoTomatoClock.ViewModel
+{
+    public class TomatoClockViewModel : ViewModelBase
+    {
+        public TomatoClockViewModel()
+        {
+            InitLocalProperty();
+            InitBindingProperty();
+            InitCommand();
+        }
+
+        private void InitLocalProperty()
+        {
+
+        }
+
+        private void InitBindingProperty()
+        {
+
+        }
+
+        private void InitCommand()
+        {
+
+        }
+    }
+}
